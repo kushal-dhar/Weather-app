b0f715250d49361734b13c46a45c7427e73b412d
kushal_dhar <kushal.dhar01@gmail.com>
2018-03-22 00:54:38 -0400

    Learning to use request

---
+++ app-01/app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+  url:'https://maps.googleapis.com/maps/api/geocode/json?address=700%20health%20science%20drive&key=AIzaSyAeaQpQxy8-6s9XcAjz90MCqsxGuBnemJs',
+  json: true
+}, (error, response, body) => {
+  console.log(body);
+});

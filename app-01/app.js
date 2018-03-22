47183381939649e0fea33cd12a1c398ff1bb86b2
kushal_dhar <kushal.dhar01@gmail.com>
2018-03-22 00:57:50 -0400

    stringified http output

--- app-01/app.js
+++ app-01/app.js
@@ -4,5 +4,5 @@ request({
   url:'https://maps.googleapis.com/maps/api/geocode/json?address=700%20health%20science%20drive&key=AIzaSyAeaQpQxy8-6s9XcAjz90MCqsxGuBnemJs',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
